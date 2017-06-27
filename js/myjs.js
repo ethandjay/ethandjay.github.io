10d95aad4ee010a52aaa3186cbfe1684a238dc34
Ethan Jaynes <ejaynes@wustl.edu>
2017-06-27 15:56:46 -0500

    about update, intervals for events

--- js/myjs.js
+++ js/myjs.js
@@ -1,16 +1,26 @@
-$(window).scroll(function() {
-	var scrollHeight = $(this).scrollTop();
+
+// var userScrolled = false;
+
+// $(window).scroll(function() {
+//   userScrolled = true;
+// });
+
+// setInterval(function() {
+//   if (userScrolled) {
+
+var scrollEvents = function() {
+	var scrollHeight = $(window).scrollTop();
 	$('.section').each(function(index) {
 		$(this).css({
 			opacity: function () {
 				var windowHeight = $(window).height();
 				var elemHeight = $(this).offset().top;
-				//if(index == 0) $('.nav-link').text(1 - Math.abs(((elemHeight - scrollHeight) / windowHeight)*2-1));
+				// if(index == 0) $('.nav-link').text(1 - Math.abs(((elemHeight - scrollHeight) / windowHeight)*2-1));
 				return 1 - Math.abs(((elemHeight - scrollHeight) / windowHeight)*2-.5) + .5;
 			}
 		})
 	})
-	if ($(this).scrollTop() > 50) {
+	if ($(window).scrollTop() > 50) {
 		$('.carousel-overlay').fadeOut();
 	} else {
 		$('.carousel-overlay').fadeIn(200);
@@ -19,8 +29,16 @@ $(window).scroll(function() {
 	$('.overfit').css('transform', function () {
 		var val = 10 - (1.5*(($(document).height() - scrollHeight) / $(document).height())-.5)*10;
 		return "translateY(" + -val + "vh)";
-	})
-})
+	});
+}
+
+$(document).ready(scrollEvents);
+setInterval(scrollEvents, 10);
+
+
+//     userScrolled = false;
+//   }
+// }, 10);
 
 $(document).on('click', '.nav-link', function(event){
 
