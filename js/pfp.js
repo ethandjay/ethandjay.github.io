9f4607d98aad3bbb8e845a86734e809eca793062
Ethan Jaynes <ethandjay@gmail.com>
2017-10-25 15:17:47 -0500

    layout isn't absolute (ass) anymore

--- js/pfp.js
+++ js/pfp.js
@@ -1,6 +1,3 @@
 $(document).ready( function() {
-	$('.logo-wrapper').css('margin-top', ($(window).height() / 5) - ($('.logo-wrapper').height() / 2));
-	console.log("fuck");
-
-	$('.stream').on('click', function() {$('.player').toggleClass('min')});
+	$('.stream').on('click', function() { $('.player').toggleClass('min') });
 });
