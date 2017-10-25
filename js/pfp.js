5a0602c948aefc6e38de2df1f422bd461ad161f2
Ethan Jaynes <ethandjay@gmail.com>
2017-10-25 16:16:16 -0500

    janky js positioning, fat trimming

--- js/pfp.js
+++ js/pfp.js
@@ -1,3 +1,14 @@
+function recenter() {
+	var m = ($(window).height() - ($('.container').height() + 180))/2;
+	$('.container').css({ "margin-top" : m<0?0:m });
+}
+
+
+$(window).resize( function() { recenter(); } );
+
 $(document).ready( function() {
-	$('.stream').on('click', function() { $('.player').toggleClass('min') });
+	$('.stream').on('click', function() { 
+		$('.player').toggleClass('min');
+	});
+	recenter()
 });
